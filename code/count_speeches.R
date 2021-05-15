bb1e9c64483d123f50873e4aa8d6d150894de2be
judgelord <devin.jl@gmail.com>
2021-05-15 16:53:25 -0500

    district mention counts

--- code/count_speeches.R
+++ code/count_speeches.R
@@ -58,8 +58,8 @@ d %<>% mutate(file_size = here('data', 'txt', year, icpsr, file_name) %>%
 head(d)
 
 # filter out small files
-d %<>% filter(file_size > 1000)
-dim(d)
+#d %<>% filter(file_size > 1000)
+#dim(d)
 
 # nchar
 d %<>% 
@@ -78,47 +78,55 @@ d$nchar %>% max()
 # save
 d_files <- d
 save(d_files, file = here("data", "d_files.Rdata"))
-
+## RESTORE FROM SAVED 
+# load(here("data", "d_files.Rdata"))
+d <- d_files
 
 # Merge in metadata 
 load(here("data", "cr_metadata.Rdata"))
 head(cr_metadata)
 
-legislation <- "ACT|BILL|RESOLUTION|AMMENDMENT|EARMARK|SPONSORS"
+legislation_strings <- "ACT|BILL|RESOLUTION|AMENDMENT|EARMARK|APPROPRIATIONS|AUTHORIZATION|BUDGET|SPONSORS|WITHDRAWAL|PROVIDING FOR CONGRESSIONAL DISAPPROVAL OF A RULE SUBMITTED"
 
-business <- "PRAYER|PLEDGE OF ALLEGIANCE|MORNING BUSINESS|MESSAGE|CONDEMING|APPOINTMENT|NOMINATION|CONFIRMATION|REPORT|PETITION|MEMORIAL|COMMUNICATION"
+business_strings <- "PRAYER|PLEDGE OF ALLEGIANCE|MORNING BUSINESS|MESSAGE|PRIVILEGES OF THE|CONDEMING|APPOINTMENT|NOMINATION|CONFIRMATION|REPORT|PETITION|MEMORIAL|COMMUNICATION| MONTH|SCHEDULE|LEAVE OF ABSENCE|GENERAL LEAVE|ELECTING|RESIGNATION|MOMENT OF SILENCE"
   
-process <- "RECOGNIZED FOR [0-9] MINUTES|UNANIMOUS CONSENT|ADJOURNMENT|PETITION| ORDER|^ORDER|MOTION|RECESS|CALENDAR|RECOGNITION|SPEAKER PRO TEMPORE"
+process_strings <- "RECOGNIZED FOR [0-9] MINUTES|ANNOUNCEMENT|RESERVATION OF LEADER TIME|UNANIMOUS CONSENT|ADJOURNMENT|EXECUTIVE SESSION|PETITION| ORDER|^ORDER|MOTION|RECESS|CALENDAR|RECOGNITION|WELCOMING|OATH |SPEAKER PRO TEMPORE|MEASURES DISCHARGED|INTENT TO OBJECT"
   
 
-# clean up headers 
+# clean up headers into type and subtype 
 cr_metadata %<>% 
   mutate(header = header %>% toupper(),
-         legislation = str_extract(header, legislation),
-         business = str_extract(header, business),
-         proess = str_extract(header, process),
+         legislation = str_extract(header, legislation_strings),
+         business = str_extract(header, business_strings),
+         process = str_extract(header, process_strings),
          subtype = coalesce(legislation, business, process, header) %>% 
            str_remove(";.*") %>% 
-           str_remove_all(" BY .*| UNTIL.*| \\(EXECUTIVE.*")
+           str_remove_all(" BY .*| UNTIL.*| \\(EXECUTIVE.*"),
+         type = ifelse(!is.na(process), "process", "other"),
+         type = ifelse(!is.na(business), "business", type),
+         type = ifelse(!is.na(legislation), "legislation", type)
   )
 
-
-to_exclude <- str_c(business, process, sep = "|") %>% str_split("") 
-
-cr_metadata %<>% filter(!subtype %in% to_exclude )
-
+# inspect
+cr_metadata$type %>% head()
+cr_metadata$subtype %>% head()
 count(cr_metadata, subtype, sort = T) %>% head(100) %>% kable(format = "pipe")
+count(cr_metadata %>% filter(type == "other"), subtype, sort = T) %>% head(100) %>% kable(format = "pipe")
+
 
 # Merge 
 cr_metadata %<>% rename(file_htm = file)
-
-cr_metadata$file_htm %>% head()
 
 # from file name to cr htm file name
 d %<>% mutate(file_htm = file_name %>% 
   str_replace("-[0-9]+-[0-9]+.txt", ".htm") )
 
-d %<>% left_join(cr_metadata)
+d %<>% left_join(cr_metadata) %>% mutate(type = replace_na(type, "other"))
 
-d %>% select(header)
+d %>% count(type)
 
+d %>% group_by(type) %>% summarise(average_nchar = mean(file_size)) %>% arrange(-average_nchar)
+
+# save
+d_meta <- d %>% select(year, congress, icpsr, state_abbrev, n_speeches, file_name, file_size, url, type, subtype) 
+save(d_meta, file = here("data", "d_meta.Rdata"))
